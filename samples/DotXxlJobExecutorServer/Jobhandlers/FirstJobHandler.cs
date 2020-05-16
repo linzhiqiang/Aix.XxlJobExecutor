42a7c516d5e5c297da6a000479fcdc3b8431ab02
linzhiqiang <360779475@qq.com>
2020-05-16 17:32:14 +0800

    添加sample

---
+++ samples/DotXxlJobExecutorServer/Jobhandlers/FirstJobHandler.cs
@@ -0,0 +1,52 @@
+﻿using DotXxlJobExecutor.DTO;
+using DotXxlJobExecutor.JobHandlers;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotXxlJobExecutorServer.Jobhandlers
+{
+    [JobHandlerAttrbute(Name = "firstJobHandler", IsAsync = false)]
+    public class FirstJobHandler : IJobHandler
+    {
+        ILogger<FirstJobHandler> _logger;
+
+        public FirstJobHandler(ILogger<FirstJobHandler> logger)
+        {
+            _logger = logger;
+
+        }
+        public async Task<ReturnT> Execute(JobExecuteContext context)
+        {
+            var result = ReturnT.Success("success");
+            try
+            {
+                //await Task.Delay(5000);
+                _logger.LogInformation("firstJobHandler执行了{0}, {1}","1",2);
+                _logger.LogInformation("firstJobHandler执行了{a}, {b}", "1", 2); //只占位符
+                await Task.CompletedTask;
+                //return ReturnT.Failed("错处啦");
+            }
+            catch (BizException) //业务异常
+            {
+
+            }
+            catch (Exception ex)
+            {
+                //只有系统异常返回错误，便于重试
+                result = ReturnT.Failed(ex.Message);
+            }
+
+            return result;
+
+
+        }
+
+        public class BizException : Exception
+        {
+
+        }
+    }
+}
---
+++ samples/DotXxlJobExecutorServer/Startup.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DotXxlJobExecutor;
+using DotXxlJobExecutor.JobHandlers;
+using DotXxlJobExecutorServer.Jobhandlers;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace DotXxlJobExecutorServer
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        /// <summary>
+        /// ≈‰÷√“¿¿µ◊¢»Î
+        /// </summary>
+        /// <param name="services"></param>
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            #region xxljob
+            services.AddXxlJob(new XxlJobOption
+            {
+                ExecutorName = "demo",
+                Token = "demo",
+                XxlJobAdminUrl = "http://localhost:8080/xxl-job-admin/",
+                ExecutorUrl = "http://localhost:5000/api/xxljob/"
+            });
+            services.AddSingleton<IJobHandler, DefaultJobHandler>();
+            services.AddSingleton<IJobHandler, FirstJobHandler>();
+
+            #endregion
+        }
+
+        /// <summary>
+        /// ≈‰÷√httpπ‹µ¿
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="env"></param>
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            #region xxljob
+            app.UseXxlJob();
+            #endregion
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
